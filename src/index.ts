7761c7ffb71dfd1a1a053bc7dc26e73bce8e021f
Rizumu Ayaka <rizumu@ayaka.moe>
2026-03-10 19:36:17 +0800

    feat: basic process

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export const foo = 'foo'
-
-export function fn(): void {
-  return
-}
+export {}
---
+++ src/type.ts
@@ -0,0 +1,53 @@
+export type AppContext<Options> = {
+  provider: Provider
+  log: LogTool
+  options: Options
+}
+
+export type Provider = {
+  // does it exist?
+  checkExistence: () => Promise<{
+    exists: boolean
+  }>
+
+  listCatalogs: () => Promise<{
+    /**
+     * catalog name -> (deps name : deps version)
+     */
+    catalogs: Record<string, Record<string, string>>
+  }>
+
+  addCatalog: (options: {
+    catalogName: string
+    depName: string
+    depVersion: string
+  }) => Promise<void>
+
+  listPackages: () => Promise<{
+    packages: RepoPackageItem[]
+  }>
+
+  addDependency: (options: {
+    directory: string
+    depName: string
+    depVersion: string
+    isCatalog: boolean
+    dev: boolean
+    peer: boolean
+  }) => Promise<void>
+
+  runInstall: (options: { packageDirectory?: string }) => Promise<void>
+}
+
+type RepoPackageItem = {
+  name: string
+  directory: string
+  description: string
+  dependencies: Record<string, string>
+  devDependencies: Record<string, string>
+}
+
+export type LogTool = {
+  info: (message: string) => void
+  error: (message: string) => void
+}
